681d9b2f6822001a4ec2b11f042303d7defa6640
Rafael Almeida <rafaelpbcp@gmail.com>
2023-01-21 15:44:25 -0500

    feat: added way to add styling to the body individually

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,13 @@ import type { AppProps } from "next/app";
 import Script from "next/script";
 import Navbar from "../components/layout/navbar";
 import { AppWrapper } from "../context/state";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.body.style.backgroundColor = pageProps.backgroundColor;
+  });
+
   return (
     <AppWrapper>
       <Script src="/js/bootstrap.bundle.min.js" />
