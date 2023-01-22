f05f5ee3d87e6812ff741ae0d2be98c3ca6b4814
dlewis123 <80225948+dlewis123@users.noreply.github.com>
2023-01-22 18:25:22 -0500

    updated help page images and restyled the page

--- components/help/guide.tsx
+++ components/help/guide.tsx
@@ -3,7 +3,81 @@ import { useAppContext } from "../../context/state";
 export default function HelpHowToGuide() {
   return (
     <>
-      <div>ahahhahaha</div>
+      <div className="container-fluid">
+        <div className="row justify-content-center my-5 ">
+          <div className="col-3 text-end d-none d-lg-block my-auto">
+            <img
+              style={{ transform: "scaleX(-1)" }}
+              src="/images/help page/arrow.svg"
+              width="60%"
+              height="auto"
+            ></img>
+          </div>
+
+          <div className="col-3 text-center my-auto">
+            <h1 className="fw-semibold text-center">Join Events</h1>
+            <h6 className="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer
+            </h6>
+          </div>
+          <div className="col-4 text-center">
+            <img
+              src="/images/help page/help_image_4.svg"
+              width="100%"
+              height="auto"
+            ></img>
+          </div>
+        </div>
+        <div className="row justify-content-center my-5 ">
+          <div className="col-4 text-center">
+            <img
+              src="/images/help page/help_image_2.svg"
+              width="100%"
+              height="auto"
+            ></img>
+          </div>
+
+          <div className="col-3 text-center my-auto">
+            <h1 className="fw-semibold text-center">Collect Points</h1>
+            <h6 className="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to
+            </h6>
+          </div>
+
+          <div className="col-3 text-start d-none d-lg-block my-auto">
+            <img
+              src="/images/help page/arrow.svg"
+              width="60%"
+              height="auto"
+            ></img>
+          </div>
+        </div>
+
+        <div className="row justify-content-center my-5 ">
+          <div className="col-xs-0 col-md-4 "></div>
+          <div className="col-3 text-center my-auto">
+            <h1 className="fw-semibold text-center">Win Prizes</h1>
+            <h6 className="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a
+            </h6>
+          </div>
+
+          <div className="col-4 text-center">
+            <img
+              src="/images/help page/help_image_5.svg"
+              width="100%"
+              height="auto"
+            ></img>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- components/help/made.tsx
+++ components/help/made.tsx
@@ -1,82 +1,79 @@
 export default function HelpHowItsMade() {
   return (
     <>
-      <div className="container">
-        <div className="row m-5">
-          <div className="col-4 text-end">
+      <div className="container-fluid">
+        <div className="row justify-content-center my-5 ">
+          <div className="col-4 text-center">
             <img
               src="/images/help page/help_image_1.svg"
-              width="300px"
+              width="100%"
               height="auto"
             ></img>
           </div>
-          <div className="col-3 text-start">
+
+          <div className="col-3 text-center my-auto">
             <h1 className="fw-semibold text-center">Technologies Used</h1>
             <h6 className="text-center">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five
+              type and scrambled it to
             </h6>
           </div>
 
-          <div className="col-4 text-start">
+          <div className="col-3 text-start d-none d-lg-block my-auto">
             <img
               src="/images/help page/arrow.svg"
-              width="280px"
+              width="60%"
               height="auto"
             ></img>
           </div>
         </div>
 
-        <div className="row m-5">
-          <div className="col-4 text-end">
+        <div className="row justify-content-center my-5 ">
+          <div className="col-3 text-end d-none d-lg-block my-auto">
             <img
               style={{ transform: "scaleX(-1)" }}
               src="/images/help page/arrow.svg"
-              width="280px"
+              width="60%"
               height="auto"
             ></img>
           </div>
 
-          <div className="col-3 text-center">
+          <div className="col-3 text-center my-auto">
             <h1 className="fw-semibold text-center">Art Tools</h1>
             <h6 className="text-center">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries,
+              ever since the 1500s, when an unknown printer
             </h6>
           </div>
-          <div className="col-4 text-start">
+          <div className="col-4 text-center">
             <img
               src="/images/help page/help_image_6.svg"
-              width="300px"
+              width="100%"
               height="auto"
             ></img>
           </div>
         </div>
 
-        <div className="row m-5">
-          <div className="col-4 text-end">
+        <div className="row justify-content-center my-5 ">
+          <div className="col-4 text-center">
             <img
               src="/images/help page/help_image_3.svg"
-              width="300px"
+              width="100%"
               height="auto"
             ></img>
           </div>
-          <div className="col-3 text-center">
+          <div className="col-3 text-center my-auto">
             <h1 className="fw-semibold text-center">Styling Tools</h1>
             <h6 className="text-center">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five
+              ever since the 1500s, when an unknown printer took a
             </h6>
           </div>
+          <div className="col-xs-0 col-md-4 "></div>
         </div>
       </div>
     </>
