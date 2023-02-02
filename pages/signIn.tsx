64234546b01a3ac32708560cb36f8d9398607770
dlewis123 <80225948+dlewis123@users.noreply.github.com>
2023-02-02 14:02:48 -0500

    restyled the sign in page and added bubbles animation

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -27,42 +27,59 @@ export default function SignInPage() {
   }
 
   return (
-    <div
-      className="container-fluid d-flex"
-      style={{
-        height: "100vh",
-        marginTop: `${-18 * 6}px`,
-      }}
-    >
-      <form className="signForm neoBorder m-auto" onSubmit={submitHandler}>
-        <h1 className="text-center fw-semibold">Log In</h1>
-        <div className="mb-3">
-          <label htmlFor="emailInput" className="form-label fw-semibold">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="emailInput"
-            ref={emailRef}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="passwordInput" className="form-label fw-semibold">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="passwordInput"
-            ref={passwordRef}
-          />
-        </div>
-        <button type="submit" className="btn eventBtnO">
-          Login
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="bubble-container2" style={{ marginTop: `${-8 * 3}px` }}>
+        <span className="bubble a"></span>
+        <span className="bubble b"></span>
+        <span className="bubble c"></span>
+        <span className="bubble d"></span>
+        <span className="bubble e"></span>
+        <span className="bubble f"></span>
+        <span className="bubble g"></span>
+        <span className="bubble h"></span>
+        <span className="bubble i"></span>
+        <span className="bubble j"></span>
+        <span className="bubble k"></span>
+      </div>
+
+      <div
+        className="container-fluid d-flex position-absolute bottom-0 left-0"
+        style={{
+          height: "100vh",
+          marginTop: `${-18 * 6}px`,
+        }}
+      >
+        <form className="signForm neoBorder m-auto" onSubmit={submitHandler}>
+          <h1 className="text-center fw-semibold">Log In</h1>
+          <div className="mb-3">
+            <label htmlFor="emailInput" className="form-label fw-semibold">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="emailInput"
+              ref={emailRef}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="passwordInput" className="form-label fw-semibold">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="passwordInput"
+              ref={passwordRef}
+            />
+          </div>
+          <button type="submit" className="btn eventBtnO">
+            Login
+          </button>
+        </form>
+        y
+      </div>
+    </>
   );
 }
 
