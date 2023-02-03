3c72864f1f94ff0c6f54eeba1605fe48661c1abb
dlewis123 <80225948+dlewis123@users.noreply.github.com>
2023-02-03 11:47:49 -0500

    more help mage and QandA edits

--- pages/help.tsx
+++ pages/help.tsx
@@ -3,6 +3,7 @@ import HelpNavbar from "../components/help/navbar";
 import HelpHowItsMade from "../components/help/made";
 import HelpHowToGuide from "../components/help/guide";
 import Footer from "../components/layout/footer";
+import QNA from "../components/help/qAndA";
 
 export default function HelpPage() {
   const [currentPage, setCurrentPage] = useState("How to Guide");
@@ -12,6 +13,7 @@ export default function HelpPage() {
       <HelpNavbar setCurrentPage={setCurrentPage} />
       {currentPage == "How to Guide" && <HelpHowToGuide />}
       {currentPage == "How was it Made" && <HelpHowItsMade />}
+      <QNA />
       <Footer />
     </>
   );
